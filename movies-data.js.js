ba6f3e62ac908a16b8f8fcfff033db2dbdd42cf0
Driendf <130811818+Driendf@users.noreply.github.com>
2025-09-11 13:36:00 -0300

    Add files via upload
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

---
+++ movies-data.js.js
@@ -0,0 +1,24 @@
+// movies-data.js
+const dataCatalogo = [
+  {
+    title: "La Reina Implacable",
+    tags: ["Drama","Finalizado","Serie TV","Subtítulos","Coreano","HD"],
+    sinopsis: "Engañada por su propia madre y hermana, le arrebataron a su hijo, arruinaron su nombre y la llevaron a una muerte cruel junto a su esposo.\nHoy, con una nueva vida, vuelve más fuerte, decidida a desenmascarar sus secretos y convertir a su esposo en un hombre poderoso. ¡La revancha será despiadada!",
+    episodes: [
+      {title: "1", link: "https://driendf.github.io/T1 [036.036] (SubEsp) La Reina Implacable.html"},
+      {title: "2", link: "https://driendf.github.io/Revancha_Ep2.html"}
+    ],
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwfe-228Cz1yvd30skfHwKox-iHinZAedu8SuXEiEWAzMOv0lFZbJoc-M1saydT7qFWw47B5EAU9Q_1Z_ycgroi2dk4yAhcJay-HG388QkUlFvh9eFipp9R2QWga8JtsNcjsGI7rQWFBITJHUKNfr3nUTyEdBJiTxhGYigveFslRmrkf-Zb3PRNhgdYug/s1440/5a2ccae0-b2e3-11ef-a2d6-41216ff1602c.jpg"
+  },
+  {
+    title: "Misterio Nocturno",
+    tags: ["Suspenso","Pausado","Serie TV","Subtítulos","EEUU","HD"],
+    sinopsis: "Detectives luchan por resolver crímenes que parecen imposibles.",
+    episodes: [
+      {title: "1", link: "https://driendf.github.io/Misterio_Ep1.html"},
+      {title: "2", link: "https://driendf.github.io/Misterio_Ep2.html"},
+      {title: "3", link: "https://driendf.github.io/Misterio_Ep3.html"}
+    ],
+    img: "https://blogger.googleusercontent.com/img/b/otra_imagen3.jpg"
+  }
+];
