688291bf7bbc689f8b34fd0e16c0f68dbe35de0a
Driendf <130811818+Driendf@users.noreply.github.com>
2025-09-11 15:04:06 -0300

    Update movies-data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- movies-data.js
+++ movies-data.js
@@ -1,20 +1,34 @@
 const moviesData = [
   {
-    title: "La Reina Implacable",
+    title: "Romance en Seúl",
+    tags: ["Romance","Finalizado","Serie TV","Subtítulos","Coreano","HD"],
+    sinopsis: "Una historia de amor entre dos jóvenes que luchan por su destino.",
+    episodes: Array.from({length:26}, (_, i) => ({
+      title: "Episodio " + (i + 1),
+      link: `https://driendf.github.io/Seul_Ep${i + 1}.html`
+    })),
+    img: "https://blogger.googleusercontent.com/img/b/otra_imagen.jpg"
+  },
+  {
+    title: "Acción Extrema",
     tags: ["Acción","Emisión","Serie TV","Doblaje","Chino","HD"],
     sinopsis: "Un agente secreto se enfrenta a peligros extremos en cada misión.",
     episodes: [
-      {title: "Episodio 1", link: "https://driendf.github.io/T1 [036.036] (SubEsp) La Reina Implacable.html"}
+      {title: "Episodio 1", link: "https://driendf.github.io/Accion_Ep1.html"},
+      {title: "Episodio 2", link: "https://driendf.github.io/Accion_Ep2.html"},
+      {title: "Episodio 3", link: "https://driendf.github.io/Accion_Ep3.html"}
     ],
-    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwfe-228Cz1yvd30skfHwKox-iHinZAedu8SuXEiEWAzMOv0lFZbJoc-M1saydT7qFWw47B5EAU9Q_1Z_ycgroi2dk4yAhcJay-HG388QkUlFvh9eFipp9R2QWga8JtsNcjsGI7rQWFBITJHUKNfr3nUTyEdBJiTxhGYigveFslRmrkf-Zb3PRNhgdYug/s1440/5a2ccae0-b2e3-11ef-a2d6-41216ff1602c.jpg"
+    img: "https://blogger.googleusercontent.com/img/b/otra_imagen2.jpg"
   },
   {
-    title: "Enmascarado: El Yerno Dominante (Doblado))",
+    title: "Misterio Nocturno",
     tags: ["Suspenso","Pausado","Serie TV","Subtítulos","EEUU","HD"],
     sinopsis: "Detectives luchan por resolver crímenes que parecen imposibles.",
     episodes: [
+      {title: "Episodio 1", link: "https://driendf.github.io/Misterio_Ep1.html"},
+      {title: "Episodio 2", link: "https://driendf.github.io/Misterio_Ep2.html"},
       {title: "Episodio 3", link: "https://driendf.github.io/Misterio_Ep3.html"}
     ],
-    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXinD6aiCjaL4_GYXlzco0lD72xa9ENG2YrmluGgmBKXu4OOWRDTk6XM5TqwFdEqQjWW63jy9bi2_iDipi1Skibktw937VipIrVUDsvmj7q-uYXyouPXw-y_4hQj46y92fQzqH6oPtQ_Xe86pnHgyPFlSPiWlVY2HfChOJxFpNVMAjsVQJ0eD5fHhzLos/s200/0000.jpg"
+    img: "https://blogger.googleusercontent.com/img/b/otra_imagen3.jpg"
   }
 ];
