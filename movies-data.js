a849e71be180ea69454fe4699b4d71b5fe32f141
Driendf <130811818+Driendf@users.noreply.github.com>
2025-09-15 06:32:26 -0300

    Update movies-data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- movies-data.js
+++ movies-data.js
@@ -1,20 +1,811 @@
-const dataCatalogo = [
+
   {
-    title: "Enmascarado: El Yerno Dominante",
-    tags: ["Acción","Emisión","Serie TV","Doblaje","Chino","HD"],
-    sinopsis: "Un agente secreto se enfrenta a peligros extremos en cada misión.",
+    title: "Tronos en la Sombra",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
     episodes: [
-      {title: "Episodio 1", link: "https://driendf.github.io/DramaBreeze2/T1 [120.120] (Latino) Enmascarado El Yerno Dominante (Doblado).html"}
+      {title: "Episodio 1", link: ""}
     ],
-    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXinD6aiCjaL4_GYXlzco0lD72xa9ENG2YrmluGgmBKXu4OOWRDTk6XM5TqwFdEqQjWW63jy9bi2_iDipi1Skibktw937VipIrVUDsvmj7q-uYXyouPXw-y_4hQj46y92fQzqH6oPtQ_Xe86pnHgyPFlSPiWlVY2HfChOJxFpNVMAjsVQJ0eD5fHhzLos/s200/0000.jpg"
+    img: ""
   },
   {
-    title: "El Despertar del Comandante (Doblado)",
-    tags: ["Suspenso","Pausado","Serie TV","Subtítulos","EEUU","HD"],
-    sinopsis: "Detectives luchan por resolver crímenes que parecen imposibles.",
+    title: "Más Allá del Tiempo - El Amor Me Trajo a Ti",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
     episodes: [
-      {title: "Episodio 1", link: "https://driendf.github.io/DramaBreeze2/T1 [090.090] (SubEsp)(Latino) El Despertar del Comandante.html"}
+      {title: "Episodio 1", link: ""}
     ],
-    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhf7hUnRdI1Rnn4bPwM0ewyMMG1c3JX80tT7RaOaVTsMkHJMG2ttPfakckN7szUt3sAqHzbMtZhJDSqypHu6-QnN7BJDtMppPfP3pNHSV8Klx2mdn3upNGHUyoEf_hfqKUMW10Yuh5sQePbpLEWu4kLGDR1XDUhKIkkkE-3YtfJjO5BbNC1ez9MzCmzoEg/s200/T1%20EP.000%20%28SubEsp%29%20El%20Despertar%20del%20Comandante.jpg"
-  }
-];
+    img: ""
+  },
+  {
+    title: "El Juego de la Venganza",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Traición del Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Ups ! El señor Lawson se equivocó de chica",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Beso Equivocado",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Una Bella en los 80",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Lazo Inesperado Mi Esposo Es un Magnate",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Volviendo en el Tiempo Camino a la Riqueza",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Regreso Triunfal Recupera su Gloria",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Tras Renacer, Me Enamoré del Tío de Mi Ex",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Nunca Te Metas con la Esposa del CEO",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Rechazada por el Alfa",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Buscador de Tesoros - Los Ojos que Todo lo Ven",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Reina del Campo de Batalla",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Amor de Familia Llévame a Casa",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Duque Poderoso",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Fachadas que se Desvanecen - Amor más Allá del Contrato",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Bésame por ultima vez",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Fortunas Reveladas Mi Esposo Es Poderoso",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Venganza De La Emperatriz",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Senderos de Esperanza - Su Viaje de Regreso a Casa",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Renacer de la Esposa Gorda",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "¡Papi, Mami, Les Leo la Mente!",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "De Regreso a los 80",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Genio incomparable",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Resurgimiento del Gran Soberano",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Realmente No Soy Inmortal",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Los Verdaderos Familiares Se Atraen entre Sí",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Lucha por el Éxito",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Contraataque De La Esposa Gorda",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Boda con el heredero desconocido",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Intriga y Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Emperatriz del Mundo del Entretenimiento",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Cuando Mis Hermanas Me Piden Perdón",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Cuando Ella Vuelve a Levantarse",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La mascarada del multimillonario",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Eres Mi Único Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Matrimonio a toda prisa, marido millonario, mimos ilimitados",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Gran Amor Materno",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Ojalá Fueras Tú",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Crisis y redención final",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Renacer en los 80 De Esposa Gorda a Reina del Éxito",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Embarazo Inesperado",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Amor Después de la Muerte",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "De Amante por Contrato a Novia del Multimillonario",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Regreso del Hombre Poderoso",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Tu lugar es a mi lado",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Heredera Multimillonaria Divorciada",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Matrimonio instantáneo con mi marido hombre lobo",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "A Estrella para toda mi vida",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Madrastra del Otro Mundo",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Vuelta a la ciudad La madre con cinco hijos sesudo",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "¿Volverás a ser mi amor？",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Yo Vengo de la Montaña",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Vuelve a la Cumbre",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Puño de hierro, corazón tierno",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Rompiendo el Hielo",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Destinos Entrelazados - Sus Recuerdos Robados",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Cuatro hermanas me rogaron que volviera a casa",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Heredera Rechazada",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Destinos Cambiados",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La doble vida de mi esposo multimillonario",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Mi Hijo, el Magnate más Rico",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Regreso de la Reina Dominante",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Regreso de la Leyenda",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Lanza Fénix (Doblado)",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "En Busca de Ella",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La venganza de la esposa XXL",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La niña del destino La eleccion de Asya",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Una Noche Para Siempre",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Mi Hija es la Suprema",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Buena Chica de la Mafia",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Valor del Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Venganza Enmascarada - En Nombre de Mi Hermana",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Recuerdos de Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Me Encanta la Manera en que Mientes",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Mi Esposo Agente Secreto",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Una Noche Destinada con Mi Jefe",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Un maestro oculto",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Contraataque de la Heredera Verdadera",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Mi capitán Alfa",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Matrimonio por Venganza y Amor Dulce",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Adiós al Pasado No Estará Ciego por el Amor",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Estafador de Bodas Mi Papá le Hace Rogar",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "La Reina Implacable",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "El Código de la Venganza",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Levántate y Brilla. Sra. Heredera",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
+  {
+    title: "Corazones Entrelazados Una Vez Más",
+    tags: ["Drama","Chino","HD"],
+    sinopsis: "Desconocido",
+    episodes: [
+      {title: "Episodio 1", link: ""}
+    ],
+    img: ""
+  },
