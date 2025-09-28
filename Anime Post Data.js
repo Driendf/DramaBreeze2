e1e5fb67b2ccbea4017580865230cc72cfec574f
Driendf <130811818+Driendf@users.noreply.github.com>
2025-09-28 13:25:18 -0300

    Update Anime Post Data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- Anime Post Data.js
+++ Anime Post Data.js
@@ -1,6 +1,7 @@
 const dataCatalogo = [
-{"title":"La Traicion del Amor","img":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcJMeJK4-SpdgCsWxq3GBP_n_hLcQMGpyhMo-5z8Yh26AXvs4d_wWWJSk3jD3yaa2bSnNNw3Lf-5CUT2Z18UbY-LgvKknhmqW2vHghdib20fzya3zeWvZfpMU3_Zz4pB8DWAIsqhItJFG1uvRAM6LacuxL9rwMSjr1ziJnCqWBaNHqJ7KXlLWu4HG2BfM/s800/T1%20EP.000%20%28Latino%29%20La%20Traici%C3%B3n%20del%20Amor.%20Fierce%20Grace.%20Braving%20the%20Betrayal.jpg","category":"drama","tags":["drama"],"sinopsis":"nada","seasons":[{"episodes":[{"title":"La Traicion del Amor","link":"https://driendf.github.io/T1 [106.106] (Latino) La Traición del Amor (Doblado).html"}]}]},
+{"title":"La Traicion del Amor","img":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcJMeJK4-SpdgCsWxq3GBP_n_hLcQMGpyhMo-5z8Yh26AXvs4d_wWWJSk3jD3yaa2bSnNNw3Lf-5CUT2Z18UbY-LgvKknhmqW2vHghdib20fzya3zeWvZfpMU3_Zz4pB8DWAIsqhItJFG1uvRAM6LacuxL9rwMSjr1ziJnCqWBaNHqJ7KXlLWu4HG2BfM/s800/T1%20EP.000%20%28Latino%29%20La%20Traici%C3%B3n%20del%20Amor.%20Fierce%20Grace.%20Braving%20the%20Betrayal.jpg","category":"anime","tags":["drama"],"sinopsis":"nada","seasons":[{"episodes":[{"title":"La Traicion del Amor","link":"https://driendf.github.io/T1 [106.106] (Latino) La Traición del Amor (Doblado).html"}]}]},
 ];
+
 
 
 
