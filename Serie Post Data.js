cac4153011b184d5269633485da8b30b41ccb8c1
Driendf <130811818+Driendf@users.noreply.github.com>
2025-10-04 14:07:55 -0300

    Update Serie Post Data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- Serie Post Data.js
+++ Serie Post Data.js
@@ -1,10 +1,14 @@
 window.dataCatalogo = window.dataCatalogo || [];
-
 window.dataCatalogo.push(
 
-{"title":"Zorro","img":"https://image.tmdb.org/t/p/w154/8fofhCU3SRcRdXYL3psiW4zfyDt.jpg","category":"series","tags":["drama"],"sinopsis":"Al convertirse en alcalde de Los Ángeles de 1821, Don Diego de la Vega se enfrenta a la resistencia del corrupto empresario Don Emmanuel.. زورو","seasons":[{"episodes":[{"title":"A Estrella para toda mi vida","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}]}]},
-
-
+{"title":"Zorro","img":"https://image.tmdb.org/t/p/w154/8fofhCU3SRcRdXYL3psiW4zfyDt.jpg","category":"series","tags":["drama"],"sinopsis":"Al convertirse en alcalde de Los Ángeles de 1821, Don Diego de la Vega se enfrenta a la resistencia del corrupto empresario Don Emmanuel.. زورو","seasons":[
+{"episodes":[
+{"title":"A Estrella para toda mi vida","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"},
+{"title":"A Estrella para toda mi vida","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+]},
+{"episodes":[
+{"title":"A Estrella para toda mi vida","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"},
+{"title":"A Estrella para toda mi vida","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+]}]},
 );
-
 
