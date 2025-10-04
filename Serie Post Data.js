63b48ee5cc7582e16d521aefc083112be58fe5d0
Driendf <130811818+Driendf@users.noreply.github.com>
2025-10-04 14:13:55 -0300

    Update Serie Post Data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- Serie Post Data.js
+++ Serie Post Data.js
@@ -4,15 +4,15 @@ window.dataCatalogo.push(
 {"title":"Zorro","img":"https://image.tmdb.org/t/p/w154/8fofhCU3SRcRdXYL3psiW4zfyDt.jpg","category":"series","tags":["drama"],"sinopsis":"Al convertirse en alcalde de Los Ángeles de 1821, Don Diego de la Vega se enfrenta a la resistencia del corrupto empresario Don Emmanuel.. زورو","seasons":[
 {"episodes":[
 {"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"},
-{"title":"Zorro","link2":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
 ]},
 {"episodes":[
 {"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"},
-{"title":"Zorro","link2":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
-{"title":"Zorro","link3":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
-{"title":"Zorro","link4":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
-{"title":"Zorro","link5":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
-{"title":"Zorro","link6":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
+{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
 ]}]},
 );
 
