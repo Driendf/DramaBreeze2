3b750d45fbef63718c6761a70450508701b9655e
Driendf <130811818+Driendf@users.noreply.github.com>
2025-10-04 14:21:19 -0300

    Update Serie Post Data.js
    Signed-off-by: Driendf <130811818+Driendf@users.noreply.github.com>

--- Serie Post Data.js
+++ Serie Post Data.js
@@ -3,7 +3,11 @@ window.dataCatalogo.push(
 
 {"title":"Zorro","img":"https://image.tmdb.org/t/p/w154/8fofhCU3SRcRdXYL3psiW4zfyDt.jpg","category":"series","tags":["drama"],"sinopsis":"Al convertirse en alcalde de Los Ángeles de 1821, Don Diego de la Vega se enfrenta a la resistencia del corrupto empresario Don Emmanuel.. زورو","seasons":[
 {"episodes":[
-{"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"},
+{
+  "title": "Zorro",
+  "link": "https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html",
+  "link2": "https://driendf.github.io/T1 [075.075] (SubEsp) La Venganza del Zorro.html"
+}
 {"title":"Zorro","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}
 ]},
 {"episodes":[
